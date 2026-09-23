76835e536b59fd9e868a2997a8d045f41acde561
Valentino <v.villarreal@alumno.um.edu.ar>
2026-09-23 20:53:09 -0300

    Update comments for clarity in Online_Store.py

--- Online_Store.py
+++ Online_Store.py
@@ -18,11 +18,11 @@ con.commit()
 #Now it's important make an ID automatic generator for be able to
 #storage the information of every costumer log_in in the Online Store
 #and print the results to check it
-#It's not necesary...
+#It's not necesary the print...
 #Ahora es importante hacer un generador automatico de ID para poder
 #almacenar la información de cada cliente que inicie sesión en la Tienda Online
 #e imprimir los resultados para revisarlos
-#No es necesario...
+#No es necesario la impresión...
 
 for fila in cur.execute("SELECT *FROM Clients"):
     print(fila)
